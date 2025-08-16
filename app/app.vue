a63523526759feb3015d30be0a17d61ab52fd096
Kay <amerk86@gmail.com>
2025-08-16 13:14:52 +0200

    feat: Added favicon

--- app/app.vue
+++ app/app.vue
@@ -6,3 +6,23 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+useHead({
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon-96x96.png",
+      sizes: "96x96",
+    },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+  ],
+  meta: [{ name: "apple-mobile-web-app-title", content: "WildAmer" }],
+});
+</script>
